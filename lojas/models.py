5185918590979eec1f31957d1bf8bbbcdbf6100e
Rennan Maciel <renmcoliver@gmail.com>
2016-03-27 17:31:52 -0300

    Listagem de produtos

--- lojas/models.py
+++ lojas/models.py
@@ -5,17 +5,22 @@ from django.db import models
 
 class Produto(models.Model):
     nome = models.CharField(max_length=200)
-    quantidade = models.IntegerField()
+    quantidade = models.IntegerField(default=1)
+    def __str__(self):
+        return self.nome + ' (' + str(self.quantidade) + ')'
 
 
 class Carrinho(models.Model):
-    pass
+    def __str__(self):
+        return self.id
 
 
 class ItemCarrinho(models.Model):
     carrinho = models.ForeignKey(Carrinho, on_delete=models.CASCADE)
     produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
-    quantidade = models.IntegerField()
+    quantidade = models.IntegerField(default=1)
+    def __str__(self):
+        return self.produto.nome + ' (' + str(self.quantidade) + ')'
 
 
 class Pedido(models.Model):
@@ -25,8 +30,9 @@ class Pedido(models.Model):
 class ItemPedido(models.Model):
     pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
     produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
-    quandidade = models.IntegerField()
-
+    quandidade = models.IntegerField(default=1)
+    def __str__(self):
+        return "Pedido %d; produto: %s; quantidade: %d" % (self.pedido.id,self.produto.nome,self.quantidade)
 
 
 
--- lojas/views.py
+++ lojas/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
+from .models import Produto
+
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("Bem vindo a lojinha")
+    produtos = Produto.objects.all()
+    context = {'produtos': produtos}
+    return render(request, 'lojas/index.html', context)
+
+def detail(request, produto_id):
+    return HttpResponse("Produto %s" % produto_id)
 
 # Create your views here.
